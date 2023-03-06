4bc946f208fb4c033887303e028de4df68fe7d39
jojeongbin <cjb1009@naver.com>
2023-03-06 14:30:13 +0900

    AppTestRunner 도입해서, 중복제거

--- src/test/java/ll/AppTests.java
+++ src/test/java/ll/AppTests.java
@@ -38,13 +38,7 @@ public class AppTests {
     @Test
     @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
     public void t3() {
-        Scanner sc = TestUtil.genScanner("종료");
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==")
@@ -56,16 +50,8 @@ public class AppTests {
     @Test
     @DisplayName("잘못된 명령어 입력에 대한 처리")
     public void t4() {
-        Scanner sc = TestUtil.genScanner("""
-                종료2
-                종료
-                """.stripIndent().trim());
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("종료2");
 
         assertThat(rs)
                 .contains("올바르지 않은 명령입니다.");
